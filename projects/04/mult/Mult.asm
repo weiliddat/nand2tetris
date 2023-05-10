6e40a38105352b1112c72169ca9bf1b51abeefb0
weiliddat <ongchiawei@gmail.com>
2023-05-10 17:44:19 +0200

    04 mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,41 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+    // reset result R2 = 0
+    @R2
+    M=0
+
+    // loop counter n = 0
+    @n
+    M=0
+
+(LOOP)
+
+    // if (n >= R0) goto END
+    @n
+    D=M
+    @R0
+    D=D-M
+    @END
+    D;JGE
+
+    // n = n + 1
+    @n
+    M=M+1
+
+    // R2 = R2 + R1
+    @R2
+    D=M
+    @R1
+    D=D+M
+    @R2
+    M=D
+
+    // goto LOOP
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
