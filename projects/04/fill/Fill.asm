ed376d6fd9b50baa24119e531772454e8e877c90
weiliddat <ongchiawei@gmail.com>
2023-05-10 17:43:57 +0200

    04 Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,103 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+    // reset screen
+    @SCREEN
+    M=0
+
+(LOOP)
+    @KBD
+    D=M
+
+    @BLACK
+    D;JGT
+
+    @WHITE
+    D;JEQ
+
+    // goto LOOP
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
+
+(BLACK)
+    // init fill size, 256 * 32
+    @8192
+    D=A
+    @s
+    M=D
+
+    // init fill index
+    @i
+    M=0
+
+    (BLOOP)
+        // if i >= s goto BLOOPEND
+        @i
+        D=M
+        @s
+        D=D-M
+        @BLOOPEND
+        D;JGE
+
+        // blacken screen + i 
+        @SCREEN
+        D=A
+        @i
+        D=D+M
+        A=D
+        M=-1
+
+        // i = i + 1
+        @i
+        M=M+1
+
+        @BLOOP
+        0;JMP
+
+    (BLOOPEND)
+        @LOOP
+        0;JMP
+
+(WHITE)
+    // init fill size, 256 * 32
+    @8192
+    D=A
+    @s
+    M=D
+
+    // init fill index
+    @i
+    M=0
+
+    (WLOOP)
+        // if i >= s goto WLOOPEND
+        @i
+        D=M
+        @s
+        D=D-M
+        @WLOOPEND
+        D;JGE
+
+        // blacken screen + i 
+        @SCREEN
+        D=A
+        @i
+        D=D+M
+        A=D
+        M=0
+
+        // i = i + 1
+        @i
+        M=M+1
+
+        @WLOOP
+        0;JMP
+
+    (WLOOPEND)
+        @LOOP
+        0;JMP
